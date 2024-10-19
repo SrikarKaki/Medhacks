db169b3d96df9763dd15dceda8972ad9e0a3a81f
Srikar <166258725+SrikarKaki@users.noreply.github.com>
2024-10-19 13:03:32 -0500

    Update Medhak.py

--- Medhak.py
+++ Medhak.py
@@ -1,3 +1,9 @@
 import streamlit as st
-st.title("this is our app")
-st.write
+
+
+left, right = st.columns(2)
+if left.button("Login with Existing Account", use_container_width=True):
+  st.write("Login")
+if right.button("Create New Account", use_container_width=True):
+  st.write("create new account")
+
